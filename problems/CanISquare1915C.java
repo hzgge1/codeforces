9b1e4202fd14a14accbd2d236efc551b8426b98b
hzgge1 <hzgge1@iscte-iul.pt>
2024-11-07 18:08:04 +0000

    resolve some problems

---
+++ problems/CanISquare1915C.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class CanISquare1915C {
+
+	public static class Square{
+		
+		private double area;
+		
+		public Square() {
+			this.area = 0;
+		}
+		
+		public void addArea(double n) {
+			this.area += n;
+		}
+		
+		public boolean canBeSquare() {
+			double sqrt = Math.sqrt(area);
+			return sqrt == (int)sqrt;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int lines = sc.nextInt();
+		while(lines-- > 0) {
+			int n = sc.nextInt();
+			Square s = new Square();
+			while(n-- > 0) {
+				s.addArea(sc.nextInt());
+			}
+			System.out.println(s.canBeSquare()? "YES":"NO");
+		}
+		sc.close();
+	}
+	
+}
---
+++ problems/TenWordsOfWisdom1850B.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TenWordsOfWisdom1850B {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int lines = sc.nextInt();
+		while(lines-- > 0) {
+			int n = sc.nextInt();
+			int index = 0;
+			double max = 0;
+			for (int i = 0; i < n; i++) {
+				double a = sc.nextInt(), b = sc.nextInt();
+				if(a <= 10 && b > max) {
+					max = b;
+					index = i+1;
+				}
+			}
+			System.out.println(index);
+		}
+		sc.close();
+	}
+
+}
