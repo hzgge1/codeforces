717a60cf1681854c84936b952d470bc5f1301f4a
hzgge1 <hzgge1@iscte-iul.pt>
2024-11-14 17:40:43 +0000

    rename and tidy

--- problems/problems601_700/Elephant617A.java
+++ problems/problems601_700/Elephant617A.java
@@ -1,6 +1,7 @@
+package problems601_700;
 import java.util.Scanner;
 
-public class Elephant {
+public class Elephant617A {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
